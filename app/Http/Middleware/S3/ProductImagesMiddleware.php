a79db184f6b84dd7568d7bb0ce363504d9429b7a
Eric Morales <eric@ikonoverde.com>
2024-04-24 12:39:33 -0600

    Refactor handling of base images in middleware - Check for existence of base image before updating URL.

--- app/Http/Middleware/S3/ProductImagesMiddleware.php
+++ app/Http/Middleware/S3/ProductImagesMiddleware.php
@@ -15,7 +15,9 @@ class ProductImagesMiddleware
         if (get_class($response) === JsonResponse::class) {
             $data = $response->getData(true);
             foreach ($data['records'] as $i => $record) {
-                $data['records'][$i]['base_image'] = $record['base_image'] ?? Storage::url($record['base_image']);
+                if (isset($record['base_image'])) {
+                    $data['records'][$i]['base_image'] = Storage::url($record['base_image']);
+                }
             }
             $response->setData($data);
         }
